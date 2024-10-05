aff760c22643e0da5e9703e45c0ce3625fde266c
William <william@Williams-MacBook-Air.local>
2024-10-05 16:46:37 -0400

    added spawn functions to creature container, need to make creatures visible

--- Scripts/creature_container.gd
+++ Scripts/creature_container.gd
@@ -2,14 +2,26 @@ extends Node2D
 
 enum CreatureType {CreatureA, CreatureB, CreatureC }
 
-var CreatureScenes = {
+var creature_scenes = {
 	CreatureType.CreatureA: preload("res://scenes/Creatures/bob_omb.tscn")
 }
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
+# Function to spawn a creature
+func spawn_creature(creature_type: CreatureType, position: Vector2):	
+	if	creature_type in creature_scenes:	
+		var creature_instance = creature_scenes[creature_type].instantiate()
+		add_child(creature_instance)
+		creature_instance.position = position
+		print("Creature type summoned: ", creature_type)
+		return creature_instance
+	else:
+		print("Creature type not found: ", creature_type)
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_pressed("down"):	
+		spawn_creature(CreatureType.CreatureA, Vector2(20, 39))
 	pass
