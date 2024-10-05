8a941493b7d8113fd523446a792b29a847365c68
felix <f.a.r.barrette@hotmail.com>
2024-10-05 12:48:06 -0400

    added card hover and card now emit on left click

--- Scripts/card.gd
+++ Scripts/card.gd
@@ -9,6 +9,10 @@ extends Node2D
 @onready var description_lbl = $CardDescription/DescriptionLbl
 @onready var name_lbl = $Name/NameLbl
 
+signal card_activated
+
+var mouse_hover = false
+
 
 func _ready():
 	set_values(cost, card_name, description)
@@ -22,4 +26,16 @@ func set_values(_cost, _name, _description):
 	name_lbl.text = card_name
 
 func _process(_delta):
-	pass
+	if mouse_hover:
+		scale = Vector2(1.2, 1.2)
+	else:
+		scale = Vector2(1, 1)
+	if Input.is_action_just_pressed("LeftClick") && mouse_hover:
+		print("clicked")
+		card_activated.emit()
+
+func _on_mouse_hover_mouse_entered():
+	mouse_hover = true
+
+func _on_mouse_hover_mouse_exited():
+	mouse_hover = false
--- Scripts/card_hand.gd
+++ Scripts/card_hand.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var card = preload("res://scenes/card.tscn")
 @onready var position_decay := 0
+@onready var spawn_point = $CanvasLayer/SpawnPoint
 
 func _ready():
 	pass # Replace with function body.
@@ -12,13 +13,15 @@ func _process(_delta):
 func _on_generate_card_1_pressed():
 	var new_card = card.instantiate()
 	add_child(new_card)
+	new_card.position = spawn_point.position
 	new_card.position.x += position_decay
-	position_decay += 50
+	position_decay += 100
 	new_card.set_values(9, "Shibougamo", "Lieu perdu")
 
 func _on_generate_card_2_pressed():
 	var new_card = card.instantiate()
 	add_child(new_card)
+	new_card.position = spawn_point.position
 	new_card.position.x += position_decay
-	position_decay += 50
+	position_decay += 100
 	new_card.set_values(5, "Saint-Ciboire", "Lieu encore plus perdu")
