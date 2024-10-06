2550bda0df4b77dc5434466e590321182e6d720b
felix <f.a.r.barrette@hotmail.com>
2024-10-06 18:15:45 -0400

    functional checkpoint

--- Scripts/single_lane.gd
+++ Scripts/single_lane.gd
@@ -2,9 +2,24 @@ extends TileMapLayer
 
 @onready var player_spawn_point = $PlayerSpawnPoint
 @onready var ai_spawn_point = $AISpawnPoint
-
+@onready var checkpoint_container = $CheckpointContainer
 func _ready():
 	pass # Replace with function body.
 
 func _process(_delta):
 	pass
+
+func _on_checkpoint_update_checkpoint(_checkpoint_control):
+	print("checkpoint 1 now has value: ", _checkpoint_control)
+
+func _on_checkpoint_2_update_checkpoint(_checkpoint_control):
+	print("checkpoint 2 now has value: ", _checkpoint_control)
+
+func _on_checkpoint_3_update_checkpoint(_checkpoint_control):
+	print("checkpoint 3 now has value: ", _checkpoint_control)
+
+func _on_checkpoint_4_update_checkpoint(_checkpoint_control):
+	print("checkpoint 4 now has value: ", _checkpoint_control)
+
+func _on_checkpoint_5_update_checkpoint(_checkpoint_control):
+	print("checkpoint 5 now has value: ", _checkpoint_control)
