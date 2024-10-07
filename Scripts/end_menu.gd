af86f69f38e8818584e3a15f17fb40570543845b
felix <f.a.r.barrette@hotmail.com>
2024-10-06 22:41:41 -0400

    fixed bug, now starting over quit pause

--- Scripts/end_menu.gd
+++ Scripts/end_menu.gd
@@ -4,16 +4,11 @@ extends Control
 @onready var end_msg = $EndContainer/EndMessage
 @onready var retry_btn = $EndContainer/RetryBtn
 
-func _ready():
-	pass # Replace with function body.
-
-func _process(delta):
-	pass
-
 func _on_quit_btn_pressed():
 	get_tree().quit()
 
 func _on_retry_btn_pressed():
+	Engine.time_scale = 1
 	if victory:
 		get_tree().change_scene_to_file("res://scenes/deck_editor.tscn")
 	else:
